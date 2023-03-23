9b7403e21d5cd29da8c392b4518b5e0523729130
JuliaEdmunds <julia.edmunds112@gmail.com>
2023-03-24 00:16:44 +0800

    Adding tutorial
    Working on the early stage 2D tutorial version

--- Assets/Scripts/Visuals/PageSwiper.cs
+++ Assets/Scripts/Visuals/PageSwiper.cs
@@ -1,66 +1,53 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using Unity.VisualScripting;
 using UnityEngine;
 using UnityEngine.EventSystems;
 
-public class PageSwiper : MonoBehaviour, IDragHandler, IEndDragHandler
+public class PageSwiper : MonoBehaviour
 {
-    public float PercentThreshold = 0.2f;
-    public float Easing = 0.5f;
-    public int TotalPages = 3;
-
-    private int m_CurrentPage = 1;
-    private Vector3 m_PanelLocation;
-
-    private void Start()
+    [Serializable]
+    private struct PanelData
     {
-        m_PanelLocation = transform.position;
+        public int PanelIndex;
+        public GameObject Panel;
     }
 
-    public void OnDrag(PointerEventData data)
+    [SerializeField] private List<PanelData> m_PanelData;
+
+    private int m_CurrentPanelIndex = 0;
+    private int m_MaxPanelIndex = 2;
+
+    public void ForwardClick()
     {
-        float difference = data.pressPosition.x - data.position.x;
-        transform.position = m_PanelLocation - new Vector3(difference, 0, 0);
+        if (m_CurrentPanelIndex >= m_MaxPanelIndex)
+        {
+            return;
+        }
+
+        GameObject currentPanel = m_PanelData[m_CurrentPanelIndex].Panel;
+        GameObject nextPanel = m_PanelData[m_CurrentPanelIndex + 1].Panel;
+
+        nextPanel.SetActive(true);
+        currentPanel.SetActive(false);
+
+        m_CurrentPanelIndex++;
     }
 
-    public void OnEndDrag(PointerEventData data)
+    public void BackClick()
     {
-        float percentage = (data.pressPosition.x - data.position.x) / Screen.width;
-
-        if (Mathf.Abs(percentage) >= PercentThreshold)
+        if (m_CurrentPanelIndex <= 0)
         {
-            Vector3 newLocation = m_PanelLocation;
-
-            if (percentage > 0 && m_CurrentPage < TotalPages)
-            {
-                m_CurrentPage++;
-                newLocation += new Vector3(-Screen.width, 0, 0);
-            }
-            else if (percentage < 0 && m_CurrentPage > 1)
-            {
-                m_CurrentPage--;
-                newLocation += new Vector3(Screen.width, 0, 0);
-            }
-
-            StartCoroutine(SmoothMove(transform.position, newLocation, Easing));
-            m_PanelLocation = newLocation;
+            return;
         }
-        else
-        {
-            StartCoroutine(SmoothMove(transform.position, m_PanelLocation, Easing));
-        }
-    }
 
-    public IEnumerator SmoothMove(Vector3 startPos, Vector3 endPos, float seconds)
-    {
-        float t = 0f;
+        GameObject currentPanel = m_PanelData[m_CurrentPanelIndex].Panel;
+        GameObject nextPanel = m_PanelData[m_CurrentPanelIndex - 1].Panel;
 
-        while (t <= 1.0)
-        {
-            t += Time.deltaTime / seconds;
-            transform.position = Vector3.Lerp(startPos, endPos, Mathf.SmoothStep(0f, 1f, t));
-            yield return null;
-        }
+        nextPanel.SetActive(true);
+        currentPanel.SetActive(false);
+
+        m_CurrentPanelIndex--;
     }
 }
